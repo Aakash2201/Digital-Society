f10c18a276e32c3c5d9e1b0b8747b24278388a61
Vedant Jadhav <60446303+vedantjdv@users.noreply.github.com>
2021-07-10 16:53:47 +0530

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,13 +1,25 @@
 
-var log = "abc";
-var pass = "abc";
-var btn= document.querySelector("button");
+var temp;
+var tempp;
+var log = ["abc","123",temp];
+var pass = ["abc","123",tempp];
+var count=0;
+var invalidMessage = document.querySelector(".invalid-credentials");
+var btn= document.querySelector(".btn");
+var btn2= document.querySelector(".btn-2");
+
 function showpop(){
     var login = document.getElementById('login-id').value;
     var password = document.getElementById('password').value;
-    if(login =="abc" && password=="abc")
-    window.open("https://www.google.com")
-    else if(login != "abc" || password!="abc")
-    alert("Invalid Login credentials");
+    for(var i=0;i<log.length;i++)
+    {
+    if(login ==log[i] && password==pass[i])
+    {
+    window.open("https://www.google.com","_self");
+    count=1;
+    }
+} 
+if(count==0)
+    invalidMessage.style.display = "flex";
 }
-var clicked = btn.addEventListener("click",showpop)
+btn.addEventListener("click",showpop);
